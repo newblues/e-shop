bae4a46258e3d94d1397ed6998b62d9be0886cbc
Antoine Sauvage <new_blues@hotmail.com>
2019-10-29 16:54:15 +0100

    add checkeout page and checkout component, user reselec for every reducer and get dynamic url param for navigation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import Checkout from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -43,6 +44,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={Checkout} />
+
           <Route
             exact
             path='/signin'
