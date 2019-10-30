7913bea5ea36d4765664c9d534d722fef1a7f155
Antoine Sauvage <new_blues@hotmail.com>
2019-10-30 14:29:34 +0100

    fix yarn file pb with heroku

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -30,6 +30,7 @@ const CollectionItem = ({ item, addItem }) => {
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
+
 export default connect(
   null,
   mapDispatchToProps
